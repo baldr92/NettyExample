df7ac02aad27a80ffc5bb8e3c6baaed663e0e713
Paul <baldr92>
2018-11-07 23:08:45 +0300

    Initial commit

---
+++ src/main/java/DiscardServerHandler.java
@@ -0,0 +1,36 @@
+import io.netty.buffer.ByteBuf;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead (ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf inBuffer = (ByteBuf) msg;
+        String receivedMsg = inBuffer.toString(CharsetUtil.UTF_8);
+        ctx.write(Unpooled.copiedBuffer("Hello" + receivedMsg, CharsetUtil.UTF_8));
+
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
